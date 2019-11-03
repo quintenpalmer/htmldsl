f3744549fdc7887161531db56acba9f57c2e3d6f
Quinten Palmer <quintenpalmer@gmail.com>
2019-11-03 13:54:39 -0500

    Add attributes
    We will leverage these in the html next.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub fn render_simple_html_page(html: Html) -> String {
     format!("<!DOCTYPE html>{}", render_tag_element(html))
 }
@@ -16,5 +18,57 @@ pub struct Html {}
 impl TagRenderable for Html {
     fn get_name() -> String {
         "html".into()
+    }
+}
+
+trait Attribute {
+    fn attr_key(&self) -> String;
+    fn attr_value(&self) -> String;
+}
+
+pub struct Lang {
+    pub tag: LanguageTag,
+    pub sub_tag: LanguageSubTag,
+}
+
+impl Attribute for Lang {
+    fn attr_key(&self) -> String {
+        "lanq".into()
+    }
+
+    fn attr_value(&self) -> String {
+        format!("{}-{}", self.tag, self.sub_tag)
+    }
+}
+
+pub enum LanguageTag {
+    En,
+}
+
+impl fmt::Display for LanguageTag {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                LanguageTag::En => "en",
+            }
+        )
+    }
+}
+
+pub enum LanguageSubTag {
+    Us,
+}
+
+impl fmt::Display for LanguageSubTag {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                LanguageSubTag::Us => "US",
+            }
+        )
     }
 }
