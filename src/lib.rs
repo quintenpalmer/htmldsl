ebeb599cfddb2a20c2c4531cec5fef8ffc99e67a
Quinten Palmer <quintenpalmer@gmail.com>
2020-01-04 21:54:20 -0500

    Don't re-export; callers can use these modules as-is

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,13 @@
-mod attributes;
-mod element;
-mod styles;
+pub mod attributes;
+pub mod element;
+pub mod styles;
 mod traits;
-mod units;
+pub mod units;
 
-pub use attributes::*;
-pub use element::*;
-pub use styles::*;
 pub use traits::element_traits::Element;
 pub use traits::style_traits::Style;
-pub use units::*;
 
-pub fn render_simple_html_page(html: Html) -> String {
+pub fn render_simple_html_page(html: element::Html) -> String {
     format!(
         "<!DOCTYPE html>{}",
         traits::element_traits::Renderable::Tag(&html).render()
