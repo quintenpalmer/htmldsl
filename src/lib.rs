b7bdd7600e18de6ef0ad70bb48e2a7ee82c430c9
Quinten Palmer <quintenpalmer@gmail.com>
2020-01-06 16:58:08 -0500

    Rename element module to elements
    It contains all the definitions of the various elements; this naming
    convention matches the styles and attributes modules as well.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod attributes;
-pub mod element;
+pub mod elements;
 pub mod style_sheet;
 pub mod styles;
 mod traits;
@@ -9,7 +9,7 @@ pub use traits::element_traits::tag;
 pub use traits::element_traits::text;
 pub use traits::element_traits::Element;
 
-pub fn render_simple_html_page(html: element::Html) -> String {
+pub fn render_simple_html_page(html: elements::Html) -> String {
     format!(
         "<!DOCTYPE html>{}",
         traits::element_traits::Renderable::Tag(&html).render()
