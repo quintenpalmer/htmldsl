f59f41195bae670bb60e71845356dd0a5197ccb0
Quinten Palmer <quintenpalmer@gmail.com>
2020-01-08 01:10:11 -0500

    Expose pretty printing on `render_simple_html_page`

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,13 @@ pub use traits::element_traits::tag;
 pub use traits::element_traits::text;
 pub use traits::element_traits::Element;
 
-pub fn render_simple_html_page(html: elements::Html) -> String {
+pub fn render_simple_html_page(pretty: bool, html: elements::Html) -> String {
+    let tag = traits::element_traits::Renderable::Tag(&html);
     format!(
         "<!DOCTYPE html>{}",
-        traits::element_traits::Renderable::Tag(&html).render()
+        match pretty {
+            true => tag.render_pretty(),
+            false => tag.render(),
+        }
     )
 }
