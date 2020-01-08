47f1ad04926f541b226b88558461640e72ba028f
Quinten Palmer <quintenpalmer@gmail.com>
2020-01-07 22:57:51 -0500

    Increment indent if provided
    Even though we aren't using it yet.

--- src/traits.rs
+++ src/traits.rs
@@ -62,7 +62,7 @@ pub mod element_traits {
                     let attrs = tag_element.get_attributes();
                     let rendered_children = match tag_element.get_children() {
                         Ok(v) => v.into_iter().fold("".into(), |prev, curr| {
-                            format!("{}{}", prev, curr.render_helper(_indent))
+                            format!("{}{}", prev, curr.render_helper(_indent.map(|x| x + 1)))
                         }),
                         Err(s) => s,
                     };
