3a49170df93ab90a3b8a2b408f35734289f9eece
Quinten Palmer <quintenpalmer@gmail.com>
2020-04-07 20:01:39 -0400

    Simplify body/return of derive public functions

--- htmldsl_internal_derive/src/lib.rs
+++ htmldsl_internal_derive/src/lib.rs
@@ -15,31 +15,19 @@ use proc_macro::TokenStream;
 pub fn derive_tag_renderable(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as syn::DeriveInput);
 
-    // Build the output, possibly using quasi-quotation
-    let expanded = tag::impl_tag_renderable(&input);
-
-    // Parse back to a token stream and return it
-    expanded
+    tag::impl_tag_renderable(&input)
 }
 
 #[proc_macro_derive(GenericRenderable)]
 pub fn derive_generic_renderable(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as syn::DeriveInput);
 
-    // Build the output, possibly using quasi-quotation
-    let expanded = generic::impl_generic_renderable(&input);
-
-    // Parse back to a token stream and return it
-    expanded
+    generic::impl_generic_renderable(&input)
 }
 
 #[proc_macro_derive(TagRenderableName, attributes(tag_renderable_name))]
 pub fn derive_tag_renderable_name(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as syn::DeriveInput);
 
-    // Build the output, possibly using quasi-quotation
-    let expanded = name::impl_tag_renderable_name(&input);
-
-    // Parse back to a token stream and return it
-    expanded
+    name::impl_tag_renderable_name(&input)
 }
