020cdfcd60a149fd59acc16c8bf734332c1fd0b9
Quinten Palmer <quintenpalmer@protonmail.com>
2021-09-04 10:16:16 -0400

    Prefer formatting within `panic!(...)`
    After seeing the following message:
    
    ```
    warning: panic message is not a string literal
     --> htmldsl_internal_derive/src/util.rs:7:16
      |
    7 |           panic!(format!(
      |  ________________^
    8 | |             "expected tag_renderable_name attribute to be a string: `... = \"...\"`",
    9 | |         ));
      | |_________^
      |
      = note: this is no longer accepted in Rust 2021
      = note: the panic!() macro supports formatting, so there's no need for the format!() macro here
    help: remove the `format!(..)` macro call
    ```
    
    I've simplified the `panic!(...)` invocations per the notes and help.
    Thanks Rust Devs!

--- htmldsl_internal_derive/src/util.rs
+++ htmldsl_internal_derive/src/util.rs
@@ -4,8 +4,6 @@ pub fn get_string_from_lit(lit: &syn::Lit) -> String {
     if let syn::Lit::Str(ref s) = *lit {
         s.value()
     } else {
-        panic!(format!(
-            "expected tag_renderable_name attribute to be a string: `... = \"...\"`",
-        ));
+        panic!("expected tag_renderable_name attribute to be a string: `... = \"...\"`",);
     }
 }
